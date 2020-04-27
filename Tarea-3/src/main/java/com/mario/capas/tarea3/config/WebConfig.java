8cd4acf7851ec8639051ae3406f1e07e18196150
MarioDeLeonRecinos <37156087+mariocdeleon@users.noreply.github.com>
2020-04-27 17:51:22 -0600

    arreglando nombres de los paquetes por error no se puso bien

--- Tarea-3/src/main/java/com/mario/capas/tarea3/config/WebConfig.java
+++ Tarea-3/src/main/java/com/mario/capas/tarea3/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.mario.capas.tarea.config;
+package com.mario.capas.tarea3.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
